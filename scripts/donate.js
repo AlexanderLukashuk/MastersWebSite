99b716bec8c8da9cc38fd254ad1d3de73e8ce08c
AlexanderLukashuk <upiter465@gmail.com>
2022-02-16 22:29:22 +0600

    Сделан header на странице donatePage с использованием jQuery

--- scripts/donate.js
+++ scripts/donate.js
@@ -1,4 +1,4 @@
-document.getElementById("main").style.backgroundImage = "url('img/donate.png')";
+//document.getElementById("main").style.backgroundImage = "url('img/donate.png')";
 //alert(document.getElementById("main"));
 //document.getElementsByClassName("main__title").style.color = "#fff";
 
@@ -9,4 +9,57 @@ document.getElementById("main").style.backgroundImage = "url('img/donate.png')";
 //     h1Elements[i].style.color = "#fff";
 // }
 
-$(".main__title").text("Select the donation amount");
+//$(".main__title").text("Select the donation amount");
+
+var headerPage = $("<div class='page'></div>");
+var headerContainer = $("<div class='container'></div>");
+var header = $("<div class='header'></div>");
+var headerLogo = $("<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' class='header__logo'></a>");
+var headerLogoImage = $("<img class='header__logo_image' src='img/logo.jpg' alt='Logo'>");
+var headerMenu = $("<ul class='header-menu'></ul>");
+
+// for (step = 0; step < 5; step++)
+// {
+//     var li = $("<li>class='header-menu__list'</li>");
+// }
+
+let headerMenuList = [
+    
+    $("<li class='header-menu__list'><a href='index.html'>Dungeon</a></li>"),
+    $("<li class='header-menu__list'><a href='https://ru.wikipedia.org/wiki/%D0%A5%D0%B5%D1%80%D1%80%D0%B8%D0%BD%D0%B3%D1%82%D0%BE%D0%BD,_%D0%91%D0%B8%D0%BB%D0%BB%D0%B8'>The Best Master</a></li>"),
+    $("<li class='header-menu__list'><a href='#'>Why Billy</a></li>"),
+    $("<li class='header-menu__list'><a href='help.html'>Help</a></li>"),
+    $("<li class='header-menu__list'><a href='donate.html'>Donate</a></li>")
+];
+
+
+for (step = 0; step <= 5; step++)
+{
+    headerMenu.append(headerMenuList[step]);
+}
+
+var headerButtons = $("<div class='header-btns'></div>");
+
+let buttons = [
+    $("<button class='header-btns__login' onclick='window.location='logIn.html''>Log in</button>"),
+    $("<button class='header-btns__login' onclick='window.location='register.html''>Register</button>"),
+    $("<button class='btn header-btns__price'>Get $300</button>")
+];
+
+for (i = 0; i < 3; i++)
+{
+    headerButtons.append(buttons[i]);
+}
+
+// $(".container").css
+// {
+//     'background-color', 'purple',
+//     'max-width', '1500px'
+// };
+
+$(".container").css('background-color', 'purple');
+
+header.append(headerLogo).append(headerLogoImage).append(headerMenu).append(headerButtons);
+
+headerPage.append((headerContainer.append(header)));
+$("body").append(headerPage);
