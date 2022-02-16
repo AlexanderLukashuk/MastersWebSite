21e7bbdb7aee789253ef89c95da1877b4b6705d4
AlexanderLukashuk <upiter465@gmail.com>
2022-02-16 20:59:30 +0600

    Изменено background-image у страницы Donate

---
+++ scripts/donate.js
@@ -0,0 +1,12 @@
+document.getElementById("main").style.backgroundImage = "url('img/donate.png')";
+//alert(document.getElementById("main"));
+//document.getElementsByClassName("main__title").style.color = "#fff";
+
+// var h1Elements = document.getElementsByTagName("main__title");
+
+// for(var i = 0; i < h1Elements.length; i++)
+// {
+//     h1Elements[i].style.color = "#fff";
+// }
+
+$(".main__title").text("Select the donation amount");
