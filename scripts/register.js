cab7c3d32b9eacde3c7272ab47609bdd4945eba2
AlexanderLukashuk <upiter465@gmail.com>
2022-02-19 11:11:57 +0600

    Добавлено условие в скрипт register на проверску совпадения паролей

--- scripts/register.js
+++ scripts/register.js
@@ -3,19 +3,24 @@ $("#registerBtn").click(function(){
     {
         alert('The login field must be filled in.');
     }
-
+    
     if ($("#passwordInput").val() == '')
     {
         alert('The password field must be filled in.');
     }
-
+    
     if ($("#repeatPasswordInput").val() == '')
     {
-        alert('The password field must be filled in.');
+        alert('The repeat password field must be filled in.');
     }
 
     if (($("#passwordInput").val().length < 8) || ($("#passwordInput").val().length > 16))
     {
         alert('The password field must be 8-16 characters long.');
+    }
+
+    if ($("#passwordInput").val() != $("#repeatPasswordInput").val())
+    {
+        alert("Passwords don't match.");
     }
 });
